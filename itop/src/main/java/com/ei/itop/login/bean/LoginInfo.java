481159ddbd68c5d0e66f58ad706605fc93b02ecf
kfqilin <kfqilin@163.com>
2014-10-13 11:58:17 +0800

    增加登录信息业务Bean

---
+++ itop/src/main/java/com/ei/itop/login/bean/LoginInfo.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.ei.itop.login.bean;
+
+/**
+ * @author Jack.Qi
+ *
+ */
+public class LoginInfo {
+
+	private String loginCode;
+
+	private String loginPasswd;
+
+	private String identifyingCode;
+}
